7822e1074bba9a13925b26e277366e0a2097b0f2
EDU-DOMAIN\hbova <hbova@mail.pima.edu>
2017-10-19 19:12:00 -0700

    Modified script

--- Assets/Script/ObjectBend.cs
+++ Assets/Script/ObjectBend.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ObjectBend : MonoBehaviour, IScrollable {
 
     public HingeJoint HingeToBend;
+    private JointLimits joint;
     public Transform pos;
 
     public void Start() {
@@ -12,7 +13,18 @@ public class ObjectBend : MonoBehaviour, IScrollable {
     }
 
     public void OnScrolled(float scrollAmount) {
-
+        //Debug.Log(scrollAmount);
+        //joint = HingeToBend.limits;
+        //if (joint.min <= 0 && joint.min >= -90) {
+        //    joint.min -= scrollAmount * 10;
+        //    joint.max -= scrollAmount * 10;
+        //}
+        //else {
+        //    joint.min = 0f;
+        //    joint.max = 0f;
+        //    HingeToBend.limits = joint;
+        //}
+        //HingeToBend.limits = joint;
     }
     
 }
