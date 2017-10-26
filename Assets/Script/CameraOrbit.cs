f5c9705f51d8c098b793278b3c4123f83c6b4f95
Darmek <adunne1@mail.pima.edu>
2017-10-26 01:56:54 -0700

    just added a basic camera movement script to rotate around an object a set amount.

---
+++ Assets/Script/CameraOrbit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOrbit : MonoBehaviour
+{
+
+    public Transform target;
+
+    public float horizMove = 45f;
+    public float vertMove = 15f;
+
+    public float updownAmount = 10f;
+
+    public void MoveHorizontal(bool left)
+    {
+        float dir = 1;
+        if (!left)
+            dir *= -1;
+        transform.RotateAround(target.position, Vector3.up, horizMove * dir);
+
+    }
+    public void MoveVertical(bool up)
+    {
+        float dir = 1;
+        if (!up)
+            dir *= -1;
+        transform.RotateAround(target.position, transform.TransformDirection(Vector3.right) , vertMove * dir);
+
+    }
+}
---
+++ Assets/Script/InputManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+    CameraOrbit cam;
+
+    // Use this for initialization
+    void Start() {
+        cam = GetComponent<CameraOrbit>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            cam.MoveHorizontal(true);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            cam.MoveHorizontal(false);
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            cam.MoveVertical(true);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            cam.MoveVertical(false);
+        }
+    }
+}
