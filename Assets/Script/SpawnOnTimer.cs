714310f987807ab3bcff14368d9bec9ae7573c8c
Darmek <adunne1@mail.pima.edu>
2017-10-26 19:34:02 -0700

    Rebase.

--- Assets/Script/SpawnOnTimer.cs
+++ Assets/Script/SpawnOnTimer.cs
@@ -32,7 +32,6 @@ public class SpawnOnTimer : MonoBehaviour {
 
         foreach (GameObject marble in marblePool.pooledMarbles) {
             if (!marble.gameObject.activeInHierarchy && allActive) {
-                marble.GetComponent<Rigidbody>().velocity = new Vector3(0f,0f,0f);
                 marble.SetActive(true);
                 marble.transform.position = pos;
                 marble.transform.rotation = rot;
