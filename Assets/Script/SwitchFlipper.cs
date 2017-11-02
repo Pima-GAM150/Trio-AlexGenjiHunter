4fde09ccdbc025f70cbca2ef8ad8b971d35413d5
Darmek <adunne1@mail.pima.edu>
2017-11-02 14:55:09 -0700

    Major update fixed all game issues and linked all game scenes together for completing the game. split work half and half for this push with genji since we both did work on 1 computer.

---
+++ Assets/Script/SwitchFlipper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchFlipper : MonoBehaviour, IClickable {
+
+    public GameObject flipToSwitch;
+    public float leftSide;
+    public float riteSide;
+    private bool side = false;
+    private Vector3 rotate = new Vector3();
+
+    public void OnClicked(int i) {
+        side = !side;
+        rotate.z = (side)? riteSide : leftSide;
+        flipToSwitch.transform.rotation = Quaternion.Euler(rotate);
+    }
+
+	// Use this for initialization
+	void Start () {
+        rotate.z = (side) ? riteSide : leftSide;
+        flipToSwitch.transform.rotation = Quaternion.Euler(rotate);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
